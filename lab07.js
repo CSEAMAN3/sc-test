5bf607c5d9155a9d7dfb9abe6fdc49be5554fe33
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-04-11 11:33:16 +0100

    update

--- lab07.js
+++ lab07.js
@@ -1,6 +1,8 @@
 const hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
 const tableElement = document.getElementById("sales-table");
 
+// get hold of your form element by it's id
+
 const state = {
   allCookieStands: [],
 };
